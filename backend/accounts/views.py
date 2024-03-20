6be0877b9632d016b2886385821c09ab725e9b93
rintaro1029 <tomitaka1029@gmail.com>
2024-03-20 15:46:33 +0900

    create views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+# ユーザー詳細
+class UserDetailView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+    lookup_field = "uid"
