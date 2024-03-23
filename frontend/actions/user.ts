76f201d18832883ea569b74f8959f40c743f7ae2
rintaro1029 <tomitaka1029@gmail.com>
2024-03-23 18:41:25 +0900

    function update password

--- frontend/actions/user.ts
+++ frontend/actions/user.ts
@@ -244,3 +244,49 @@ export const updateUser = async ({ accessToken, name, introduction, avatar }: Up
     };
   }
 };
+
+interface UpdatePasswordProps {
+  accessToken: string;
+  currentPassword: string;
+  newPassword: string;
+  reNewPassword: string;
+}
+
+export const updatePassword = async ({
+  accessToken,
+  currentPassword,
+  newPassword,
+  reNewPassword,
+}: UpdatePasswordProps) => {
+  try {
+    const body = JSON.stringify({
+      current_password: currentPassword,
+      new_password: newPassword,
+      re_new_password: reNewPassword,
+    });
+
+    //パスワード変更
+    const apiRes = await fetch(`${process.env.API_URL}/api/auth/users/set_password/`, {
+      method: "POST",
+      headers: {
+        Authorization: `JWT ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body,
+    });
+
+    if (!apiRes.ok) {
+      return {
+        success: false,
+      };
+    }
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+    };
+  }
+};
