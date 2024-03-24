16e203dbe79b94d163eb5857f66cfcada11f2bf8
rintaro1029 <tomitaka1029@gmail.com>
2024-03-24 15:22:37 +0900

    Function "get all post"

---
+++ frontend/actions/post.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import { UserType } from "@/lib/nextauth";
+
+// 共通のAPIリクエスト
+const fetchAPI = async (url: string, options: RequestInit) => {
+  const apiUrl = process.env.API_URL;
+
+  if (!apiUrl) {
+    return { success: false, error: "API URLが設定されていません" };
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}${url}`, options);
+
+    if (!response.ok) {
+      return { success: false, error: "APIでエラーが発生しました" };
+    }
+
+    // Content-Type ヘッダーが application/json の場合のみ、JSON を解析する
+    const contentType = response.headers.get("Content-Type");
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();
+      return { success: true, data };
+    }
+
+    // データなしで成功を返す
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: "ネットワークエラーが発生しました" };
+  }
+};
+
+export interface PostType {
+  uid: string;
+  user: UserType;
+  image: string | undefined;
+  title: string;
+  content: string;
+  updated_at: string;
+  created_at: string;
+}
+
+// 投稿一覧取得
+export const getPostList = async () => {
+  const options: RequestInit = {
+    method: "GET",
+    cache: "no-store",
+  };
+
+  // 投稿一覧取得
+  const result = await fetchAPI("/api/post-list/", options);
+
+  if (!result.success) {
+    console.error(result.error);
+    return { success: false, posts: [] };
+  }
+
+  //成功したら投稿一覧を返す
+  const posts: PostType[] = result.data;
+
+  return { success: true, posts };
+};
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import { getPostList } from "@/actions/post";
+import PostItem from "@/components/post/PostItem";
 
-export default function Home() {
-  return <div>Hello</div>;
-}
+const Home = async () => {
+  //投稿一覧を取得
+  const { success, posts } = await getPostList();
+
+  if (!success) {
+    return <div className="text-center text-sm text-gray-500">投稿の取得に失敗しました</div>;
+  }
+
+  if (posts.length === 0) {
+    return <div className="text-center text-sm text-gray-500">投稿がありません</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-5">
+      {posts.map((post) => (
+        <PostItem key={post.uid} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
